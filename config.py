e8fbf772d60d2c8d1bfb85e2bfda425802f921a0
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2023-10-12 14:35:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1800))
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
-# Get this value from @FallenxBot on Telegram by /id
+# Get this value from @Starrrobot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID", 6539720313))
 
 ## Fill these variables if you're deploying on heroku.
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Payal8800/EraMusicBot",
+    "https://github.com/Payal8800/StarMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
