b47df81aa8cfdde8414f5d12a048826fcdccb883
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-03-18 18:45:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
-DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 1800))
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 180000))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @Starrrobot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6670632940))
+OWNER_ID = int(getenv("OWNER_ID", -1002118816835))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/DiillSe")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/alinasupportt")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/YkYNetworkk")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
